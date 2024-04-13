fdf2b4311d3bdc98651daff06fc45688186d85e7
pengsulong <pengsulong@foxmail.com>
2024-04-13 15:42:32 +0800

    [Description]: 【suily-module-system】新增加system的模块 [issue]: NA

---
+++ suily-module-system/suily-module-system-api/src/main/java/com/psl/boot/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2024. Suily. All rights reserved..
+ */
+
+package com.psl.boot;
+
+/**
+ * @Author: pengsulong@foxmail.com
+ * @Date: ${DATE}
+ * @Description
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
