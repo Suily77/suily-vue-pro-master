f3dbf6ebab5cc52c18db88c7efc47f3f68700b48
pengsulong <pengsulong@foxmail.com>
2024-04-14 23:48:26 +0800

    [Description]: 【suily-server】配置端口号 [issue]: NA

---
+++ suily-server/src/main/java/com/psl/boot/SuilyServerApplication.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2024. Suily. All rights reserved..
+ */
+
+package com.psl.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: pengsulong@foxmail.com
+ * @Date: ${DATE}
+ * @Description
+ */
+@SpringBootApplication
+public class SuilyServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SuilyServerApplication.class, args);
+    }
+}
