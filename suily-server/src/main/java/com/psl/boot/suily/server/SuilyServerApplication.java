c17580dd61d99b912582d04fb99608759e12b9f2
pengsulong <pengsulong@foxmail.com>
2024-04-15 00:34:34 +0800

    [Description]: 【suily-server】改包名 [issue]: NA

--- suily-server/src/main/java/com/psl/boot/suily/server/SuilyServerApplication.java
+++ suily-server/src/main/java/com/psl/boot/suily/server/SuilyServerApplication.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2024. Suily. All rights reserved..
  */
 
-package com.psl.boot;
+package com.psl.boot.suily.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
