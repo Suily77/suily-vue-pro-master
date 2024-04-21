f2924247aeea0292480948081e2f70a28ad85f36
pengsulong <pengsulong@foxmail.com>
2024-04-21 16:41:10 +0800

    [Description]: 【suily-springboot-starter-biz-tenant】修正modules的模块，创建过滤类 [issue]: NA

---
+++ suily-framework/suily-spring-boot-starter-biz-tenant/src/main/java/com/psl/boot/suily/framwork/tenant/core/security/TenantSecurityWebFilter.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2024. Suily. All rights reserved..
+ */
+
+package com.psl.boot.suily.framwork.tenant.core.security;
+
+/**
+ * @Author: pengsulong@foxmail.com
+ * @Date: 2024/4/21
+ * @Description
+ */
+public class TenantSecurityWebFilter {
+}
